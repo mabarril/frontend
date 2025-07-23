7f88bd19597b7bd7abb108ddbce2473f4d7e3874
Marcelo Alexandre Barrionuevo <mabarril@gmail.com>
2025-07-23 12:17:58 +0000

    feat: update API URL in InscricoesService, add Vercel configuration for API rewrites and headers

--- src/app/services/inscricoes.service.ts
+++ src/app/services/inscricoes.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class InscricoesService {
-  private apiUrl = '/api';
+  private apiUrl = 'localhost:21025/api';
   constructor(private http: HttpClient) { }
 
   getInscricoesDoEvento(eventoId: number) {
